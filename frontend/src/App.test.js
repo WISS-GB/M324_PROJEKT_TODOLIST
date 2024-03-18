4e75edf9ef8343bb1b504ff038ac3f90d7c3bbd1
lbischofberger <lars.kapf@icloud.com>
2024-03-18 22:09:56 +0100

    Jest-Test mit suchfunctionen

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -8,3 +8,21 @@ test('renders learn react link', () => {
   expect(linkElement).toBeVisible()
   expect(linkElement).toHaveClass("headline")
 });
+
+test('renders Add Button', () => {
+  render(<App />);
+  // Find the Button
+  const addButton = screen.getByRole('button', { name: /add/i });
+  expect(addButton).toBeInTheDocument();
+  expect(addButton).toBeEnabled();
+  expect(addButton).toHaveClass("add-button");
+});
+
+test('renders Empty ToDo List initially', () => {
+  render(<App />);
+  // Find the Empty List Message
+  const emptyListMessage = screen.getByText(/no todos yet/i);
+  expect(emptyListMessage).toBeInTheDocument();
+  expect(emptyListMessage).toBeVisible();
+  expect(emptyListMessage).toHaveClass("empty-list-message");
+});
