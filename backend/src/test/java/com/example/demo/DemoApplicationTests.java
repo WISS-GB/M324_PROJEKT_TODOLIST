e09b5544f52bf26c92af808b3579292f35783147
lbischofberger <lars.kapf@icloud.com>
2024-03-18 21:45:00 +0100

    Unit-Test1 liste bearbeiten

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,22 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+       
+        DemoApplication demoApp = new DemoApplication();
+       
+        assertEquals(0, demoApp.getTasks().size(), "Initial task list solte ler sein");
+       
+        demoApp.addTask("{\"taskdescription\":\"Task 1\"}");
+        assertEquals(1, demoApp.getTasks().size(), "Task list size solte 1 sein anch hinzufügen");
+     
+        demoApp.delTask("{\"taskdescription\":\"Task 1\"}");
+        assertEquals(0, demoApp.getTasks().size(), "Task list size solte 0 sein nach dem löschen der task");
+    }
 }
