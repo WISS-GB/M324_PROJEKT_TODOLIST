c1cb30312dbbe7969c5eb6946a567f4fc580891c
Christopher <Carl-Christopher.Biebow@wiss-edu.ch>
2025-05-22 03:18:41 +0200

    added frontend-vite

---
+++ frontend-vite/src/1App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meine alte ToDo App</h1>
+      <p>Dies ist die portierte Version mit Vite.</p>
+    </div>
+  )
+}
+
+export default App
